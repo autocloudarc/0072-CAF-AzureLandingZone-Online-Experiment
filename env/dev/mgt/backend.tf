989cbddd120ac0eeadd6d4e8e72d4fbdc2a3f88a
Preston K. Parsard <autocloudarc@outlook.com>
2021-12-18 14:00:48 -0500

    state(remote): switch to terraform cloud

--- env/dev/mgt/backend.tf
+++
@@ -1,9 +0,0 @@
-// backend state file
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tf-<rnd>-rgp-01"
-    storage_account_name = "1tfm<rnd>"
-    container_name       = "dev-tfstate"
-    key                  = "dev.tfstate"
-  }
-}
