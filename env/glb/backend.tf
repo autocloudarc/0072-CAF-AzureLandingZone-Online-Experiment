fc540b087cc38ae1aa8ed56bfce51a10dc732171
Preston K. Parsard <autocloudarc@outlook.com>
2021-12-18 14:15:44 -0500

    backend(cloud): update syntax to switch

--- env/glb/backend.tf
+++ env/glb/backend.tf
@@ -1,9 +1,20 @@
 // backend state file
 terraform {
-  backend "azurerm" {
+  
+  /* backend "azurerm" {
       resource_group_name = "tf-<rnd>-rgp-01"
       storage_account_name = "1tfm<rnd>"
       container_name = "dev-tfstate"
       key = "dev.tfstate"
   }
+  */
+
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "autocloudarc"
+    workspaces {
+      name = "cli-azr-dev-0072-all"
+    }
+  }
+
 }
