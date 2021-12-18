0e6603b24a4eac4238d4d4d2d1e1f291efd86f6a
Preston K. Parsard <autocloudarc@outlook.com>
2021-12-18 17:33:44 -0500

    state(backend): comment out to use local

--- env/glb/backend.tf
+++ env/glb/backend.tf
@@ -1,14 +1,14 @@
 // backend state file
+/*
 terraform {
 
-/*
   backend "azurerm" {
       resource_group_name = "tf-<rnd>-rgp-01"
       storage_account_name = "1tfm<rnd>"
       container_name = "dev-tfstate"
       key = "dev.tfstate"
   }
-*/
+
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "autocloudarc"
@@ -17,3 +17,4 @@ terraform {
     }
   }
 }
+*/
